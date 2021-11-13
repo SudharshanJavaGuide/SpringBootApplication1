205924ace55093fca03aa312bec278bf195552a7
Sudharshan <sudharshanjavaguide@gmail.com>
2021-11-13 09:26:34 +0530

    Project Direcotory Commit

---
+++ Git-SpringBootApplicaion1/src/main/java/com/sudharshanit/boot/GitSpringBootApplicaion1Application.java
@@ -0,0 +1,13 @@
+package com.sudharshanit.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringBootApplicaion1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringBootApplicaion1Application.class, args);
+	}
+
+}
