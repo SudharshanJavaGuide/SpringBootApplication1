dcd6cac9c33b52b47264ec79f7488bfe1bb2294f
Sudharshan <sudharshanjavaguide@gmail.com>
2021-11-13 12:51:24 +0530

    User Bean Properties Added

--- Git-SpringBootApplicaion1/src/main/java/com/sudharshanit/boot/entity/User.java
+++ Git-SpringBootApplicaion1/src/main/java/com/sudharshanit/boot/entity/User.java
@@ -1,5 +1,15 @@
 package com.sudharshanit.boot.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class User {
 
+	private Integer userId;
+	private String userName;
+	private String userEmail;
+	private String userAddress;
+	private Long userNumber;
 }
